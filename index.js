91df04b7e7ff1963fe3c2de56368834742a03413
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-15 13:23:25 +0545

    Database connected successfull.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDb = require('./config/dbConnection');
 const app = express();
 const dotenv = require('dotenv').config();
 
+connectDb();
+
 const PORT = process.env.PORT || 4000;
+
+app.use('/', (req,res) => {
+    res.send('Hello World');
+});
 
 app.listen(PORT,() => {
     console.log(`The server is running in ${PORT}` );

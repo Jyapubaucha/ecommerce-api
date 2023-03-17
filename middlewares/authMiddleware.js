94c82fc964d286d3b99f72cab84a1657d77abe8d
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-17 18:16:30 +0545

    Added middleware and get a user details as per there login token for authorization.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,1 +1,26 @@
-  
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+    let token;
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+        try {
+            if (token) {
+                const decoded = jwt.verify(token, process.env.JWT_SECRET);
+                const user = await User.findById(decoded.id);
+                req.user = user;
+                next();
+            }
+
+        }
+        catch (err) {
+            throw new Error("Not authorized! Token expired, Please login and try again");
+        }
+    } else {
+        throw new Error("Token is not attached to header.")
+    }
+});
+
+module.exports = { authMiddleware };

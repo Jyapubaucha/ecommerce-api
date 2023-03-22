536a11d6cf2cca64df35680b6b8033f1678744dd
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-22 18:50:01 +0545

    Get a single product.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,26 @@
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 
+//create new product
 const createProduct = asyncHandler(async (req, res) => {
     try {
         const newProduct = await Product.create(req.body);
         res.json(newProduct);
     }
-    catch (error){
-        throw new Error (error);
+    catch (error) {
+        throw new Error(error);
     }
 });
 
-module.exports = { createProduct };
+//get a single product with id
+const getaProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const findProduct = await Product.findById(id);
+        res.json(findProduct);
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+})
+module.exports = { createProduct, getaProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { createProduct } = require("../controllers/productController");
+const { createProduct, getaProduct } = require("../controllers/productController");
 const router = express.Router();
 
 router.post('/', createProduct);
+router.get('/:id', getaProduct);
 module.exports = router;

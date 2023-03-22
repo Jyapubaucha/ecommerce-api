dfa01df788d05550ce0471c7ca2f1e549515e69f
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-22 19:03:04 +0545

    Installed package slugify.

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 connectDb();
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser());

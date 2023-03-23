a2efa34bd23c0c74ba4dd56de9e9b57205b41d55
Jyapubaucha <Jyapubaucha21@gmail.com>
2023-03-23 11:00:20 +0545

    Added authentication only for admin role to CRUD operation of products.

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
+
 //create new product
 const createProduct = asyncHandler(async (req, res) => {
     try {
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { createProduct, getaProduct, getAllProducts, updateProduct, deleteProduct } = require("../controllers/productController");
 const router = express.Router();
 
-router.post('/', createProduct);
+//Authenticate the product and give access to update, delete product
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
+
+router.post('/', authMiddleware, isAdmin, createProduct);
 router.get('/:id', getaProduct);
-router.put('/:_id', updateProduct);
-router.delete('/:_id', deleteProduct);
+router.put('/:_id',authMiddleware, isAdmin, updateProduct);
+router.delete('/:_id',authMiddleware, isAdmin, deleteProduct);
 router.get('/', getAllProducts);
 module.exports = router;
